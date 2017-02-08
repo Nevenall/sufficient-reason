e288668efbc4c1ff9fb8957523952a65bea30fa1
nevenall <nevenall@hotmail.com>
2017-02-07 18:20:19 -0800

    task to count words in all markdown files task to spellcheck all markdown files and output just errors

---
+++ SpellCheckAllFiles.ps1
@@ -0,0 +1,9 @@
+Get-ChildItem *.md -Recurse | ForEach-Object {
+  $lineNumber = 1; 
+  write-output "File - $($_.Name)"; 
+  get-content $_ | aspell pipe --sug-mode=ultra | ForEach-Object { 
+    if( ([string]$_).StartsWith('&')){
+      Write-Output "Warn $lineNumber $_";
+    } if([string]::IsNullOrEmpty($_)) {
+      ++$lineNumber;
+    }}}
