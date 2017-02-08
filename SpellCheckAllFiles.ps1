7f7a000e9f3b689d087996c32accabf8aa53d71e
nevenall <nevenall@hotmail.com>
2017-02-07 22:00:16 -0800

    spell check tweaks

--- SpellCheckAllFiles.ps1
+++ SpellCheckAllFiles.ps1
@@ -1,9 +1,9 @@
 Get-ChildItem *.md -Recurse | ForEach-Object {
   $lineNumber = 1; 
-  write-output "File - $($_.Name)"; 
+  write-output "File - $($_.FullName)"; 
   get-content $_ | aspell pipe --sug-mode=ultra | ForEach-Object { 
     if( ([string]$_).StartsWith('&')){
-      Write-Output "Warn $lineNumber $_";
+      Write-Output "$lineNumber $_";
     } if([string]::IsNullOrEmpty($_)) {
       ++$lineNumber;
     }}}
