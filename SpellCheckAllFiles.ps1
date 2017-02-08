d703fed18e89a0251698cd8df044497d060918bd
nevenall <nevenall@hotmail.com>
2017-02-08 08:14:13 -0800

    notes on spell checking

--- SpellCheckAllFiles.ps1
+++ SpellCheckAllFiles.ps1
@@ -1,3 +1,13 @@
+
+# need to redo this so it's legitimately line by line. 
+#  we can read each line from the file, track the number and feed that line to the spell checker. 
+# we can also then put aspell in terse mode
+# except # means soemthing in pipe mode, save the personal dictionary
+# most of markdown will be a mess. 
+# so, as we pipe each line through we can just add the line number at the start, or !
+# Not started with a !, that doesn't work, 
+# we can start it with the line number of some kind neutral buffer. even a space will do. 
+
 Get-ChildItem *.md -Recurse | ForEach-Object {
   $lineNumber = 1; 
   write-output "File - $($_.FullName)"; 
