de1bcbf5ca1e77d3f6ac6d0f6217f489ce1bdaad
Dan Behlings <nevenall@hotmail.com>
2017-07-18 13:08:38 -0700

    html build task removes all things in html.

--- Untitled-1.ps1
+++ Untitled-1.ps1
@@ -1,1 +1,5 @@
-gci *.md -Recurse | join-path "c:\"
+Get-ChildItem *.md -Recurse | Select-Object FullName | ForEach-Object { $_.FullName.Replace("SufficientReason","SufficientReason\html") -replace ".md",".html" }
+
+# we want to get the part of the path tht starts at the root of our workspace
+# so, take the file path, replace the workspace folder with workspace folder/html
+# exect will that fail because the folders don't get created? 
