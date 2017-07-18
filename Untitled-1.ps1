2f18bb04fab916796ffdbe71026bbeabeb32091d
Dan Behlings <nevenall@hotmail.com>
2017-07-18 13:13:13 -0700

    notes about making an output folder structure

--- Untitled-1.ps1
+++ Untitled-1.ps1
@@ -3,3 +3,8 @@ Get-ChildItem *.md -Recurse | Select-Object FullName | ForEach-Object { $_.FullN
 # we want to get the part of the path tht starts at the root of our workspace
 # so, take the file path, replace the workspace folder with workspace folder/html
 # exect will that fail because the folders don't get created? 
+# if we want a stuctured html folder then we have to manually create the folder structure
+# we could, alternately, append the pathing to the output filename. 
+# that would be an easy solution. 
+# or we can make a powershell module that will do the things we need. 
+# or I wonder if we can make a csx script? 
